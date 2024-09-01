158e70d6ef056bf94e5b6a19722908b8709f376a
Jasneet <jasneetpalsingh619@gmail.com>
2024-09-01 07:47:58 -0600

    added ticket form including its priority fieldset and logic

--- src/components/TicketForm.js
+++ src/components/TicketForm.js
@@ -18,5 +18,41 @@ export default function TicketForm(){
         setPriority('1');
     }
 
-    return(<></>)
+    const handleSubmit = (e) =>{
+
+        e.preventDefault();  //page will now not get reloaded cool thing I learned for it
+        clearForm();
+    }
+
+    return(
+        <form onSubmit={handleSubmit} className='ticket-form'>
+            <div>
+                <label>Title</label>
+                <input type='text' value={title} className='form-input' onChange={e => setTitle(e.target.value)}></input>
+            </div>
+
+            <div>
+                <label>Description</label>
+                <textarea type='text' value={description} className='form-input' onChange={e => setDescription(e.target.value)}></textarea>
+            </div>
+
+            <fieldset className='priority-fieldset'>
+                <legend>Priority</legend>
+                {
+                    Object.entries(priorityLabels).map(([value, label]) => (
+                    <label key={value} className='priority-label'>
+                        <input 
+                            type='radio' 
+                            value={value} 
+                            checked={priority === value} 
+                            className='priority-input'
+                            onChange={e => setPriority(e.target.value)}></input>
+                    {label}</label>
+                ))
+                }
+            </fieldset>
+
+            <button type='submit' className='button'>Submit</button>
+        </form>
+    )
 }
